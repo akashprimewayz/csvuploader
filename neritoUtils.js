caafaca3f2b2b1468eaf4f2eff97b2f528593ea5
akashprimewayz <akash.pandey@primewayz.com>
2021-10-26 15:00:52 +0530

    code cleanup

--- neritoUtils.js
+++ neritoUtils.js
@@ -1,34 +1,41 @@
+
 module.exports = {
-    successResponseJson: async function(message, code) {
-    let error = {};
-    let Error = {};
-    Error.Success = message;
-    error.isBase64Encoded = false;
-    error.statusCode = code;
-    error.headers = { 
-	    "X-Requested-With": '*',
-        "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with',
-        "Access-Control-Allow-Origin": '*',
-        "Access-Control-Allow-Methods": 'POST,GET,OPTIONS,PUT'
-    }, 
-    error.body = JSON.stringify(Error);
-    console.log(error);
-    return error;
+    months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+     csvStatus : {
+        PENDING: 'PENDING',
+        COMPLETED: 'COMPLETED',
+        FAILED: 'FAILED'
     },
-    errorResponseJson : async function (message, code) {
-    let error = {};
-    let Error = {};
-    Error.Errors = message;
-    error.isBase64Encoded = false;
-    error.statusCode = code;
-    error.headers = { 
-	    "X-Requested-With": '*',
-        "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,x-requested-with,orgId',
-        "Access-Control-Allow-Origin": '*',
-        "Access-Control-Allow-Methods": 'POST,GET,OPTIONS,PUT'
-    }, 
-    error.body = JSON.stringify(Error);
-    console.log(error);
-    return error;
+    successResponseJson: async function (message, code) {
+        let error = {};
+        let Error = {};
+        Error.Success = message;
+        error.isBase64Encoded = false;
+        error.statusCode = code;
+        error.headers = {
+            "X-Requested-With": '*',
+            "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with',
+            "Access-Control-Allow-Origin": '*',
+            "Access-Control-Allow-Methods": 'POST,GET,OPTIONS,PUT'
+        },
+            error.body = JSON.stringify(Error);
+        console.log(error);
+        return error;
+    },
+    errorResponseJson: async function (message, code) {
+        let error = {};
+        let Error = {};
+        Error.Errors = message;
+        error.isBase64Encoded = false;
+        error.statusCode = code;
+        error.headers = {
+            "X-Requested-With": '*',
+            "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,x-requested-with,orgId',
+            "Access-Control-Allow-Origin": '*',
+            "Access-Control-Allow-Methods": 'POST,GET,OPTIONS,PUT'
+        },
+            error.body = JSON.stringify(Error);
+        console.log(error);
+        return error;
     }
 }
