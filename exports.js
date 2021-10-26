d3ccbd25afa5e851856c67b89a43618e578a3777
akashprimewayz <akash.pandey@primewayz.com>
2021-10-26 17:25:16 +0530

    code clean up

--- exports.js
+++ exports.js
@@ -1,14 +1,14 @@
-let employee = require('./employee.js');
-let neritoUtils = require('./neritoUtils.js');
+const employee = require('./employee.js');
+const neritoUtils = require('./neritoUtils.js');
 const parser = require("lambda-multipart-parser");
 
 exports.handler = async function (event, ctx, callback) {
 
     try {
-        const parser = await parser.parse(event);
-        const { content, filename, contentType } = parser.files[0];
+        const csvParser = await parser.parse(event);
+        const { content, filename, contentType } = csvParser.files[0];
         let fileContent = content.toString();
-        let orgId = parser.orgId;
+        let orgId = csvParser.orgId;
 
         if (fileContent == null) {
             return neritoUtils.errorResponseJson("Please select file to upload", 400);
